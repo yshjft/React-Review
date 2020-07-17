d31f65c0ab34368f1878ca03fad95703f848757a
hys <jericoh@naver.com>
2020-07-18 01:55:11 +0900

    css start

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import './App.css';
 
-import Header from './components/Header';
+import Header from './components/Header/Header';
 import Main from './components/Main/Main';
 import Footer from './components/Footer';
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
+import './Header.css';
 
 class Header extends Component {
   render(){
     return(
-      <div id='header'>
+      <div id='header' className='header'>
         <h1>React Review</h1>
-        <h3>생활 코딩에서 배운 내용을 복습 정리 합니다</h3>
+        <h2>생활 코딩에서 배운 내용을 복습 정리 합니다</h2>
       </div>
     );
   }
